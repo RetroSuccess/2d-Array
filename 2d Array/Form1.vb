988124932856fae0316f2763a24abf8378de6520
RetroSuccess <165993473+RetroSuccess@users.noreply.github.com>
2024-06-10 13:39:03 +0200

    Add files via upload

---
+++ 2d Array/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim sum As Integer
+        sum = 0
+        Dim num(2, 2) As Integer
+        num(0, 0) = 1
+        num(0, 1) = 2
+        num(1, 0) = 3
+        num(1, 1) = 4
+
+        For i As Integer = 0 To num.GetLength(0) - 1
+            For j As Integer = 0 To num.GetLength(0) - 1
+                sum = sum + num(i, j)
+            Next
+        Next
+
+        TextBox1.Text = sum
+
+    End Sub
+End Class
